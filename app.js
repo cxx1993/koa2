b90ee359ad8085269f9379d6183cc64b7f0f94dd
chenxin <chenxin@51xf.cn>
2018-09-28 10:01:36 +0800

    将middleware和router提出，清减

--- app.js
+++ app.js
@@ -1,44 +1,11 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const Koa = require("koa");
+const app = new Koa();
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const router = require("./src/router");
 
-// error handler
-onerror(app)
+const middleware = require("./src/middleware/index"); // 引入中间件
 
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+middleware(app);
+router(app);
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
-
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
-// routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-});
-
-module.exports = app
+module.exports = app;
