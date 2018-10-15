2ca2b3be21a9a830ae462c6412a4b9cd3412427e
chenxin <chenxin@51xf.cn>
2018-10-15 16:44:12 +0800

    add 静态值code

--- src/utils/static.js
+++ src/utils/static.js
@@ -6,5 +6,19 @@
 module.exports = {
   list: {
     limit: 10
-  }
+  },
+  res_code: [
+    {
+      key: "a1",
+      value: "新增成功"
+    },
+    {
+      key: "a2",
+      value: "新增失败"
+    },
+    {
+      key: "e1",
+      value: "参数确实"
+    },
+  ]
 };
